7f2a0275ac674b29b6e4940895571e98b092287c
Braham Prakash <developer.brm@gmail.com>
2020-10-10 17:02:57 +0530

    Updates

--- precache-manifest.9a652b193ee0941de48dc996d4051bf1.js
+++ precache-manifest.9a652b193ee0941de48dc996d4051bf1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8d02f170b212c6d31ce0f0560e1ba726",
+    "revision": "0f3977d4185f9b34fcef5a909bcef590",
     "url": "/chatApp/index.html"
   },
   {
-    "revision": "010aaee7f1a748e7a7a3",
-    "url": "/chatApp/static/css/main.13fddcd0.chunk.css"
+    "revision": "38bafaf1e166ed932da1",
+    "url": "/chatApp/static/css/main.3abcf319.chunk.css"
   },
   {
     "revision": "b940c475c41f48a396ca",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chatApp/static/js/2.820b281e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "010aaee7f1a748e7a7a3",
-    "url": "/chatApp/static/js/main.ba703056.chunk.js"
+    "revision": "38bafaf1e166ed932da1",
+    "url": "/chatApp/static/js/main.3471e1ec.chunk.js"
   },
   {
     "revision": "76f1f93d7b26a5d074cb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chatApp/precache-manifest.22c34f0f6b0fd560e6b8ac8a9583780c.js"
+  "/chatApp/precache-manifest.9a652b193ee0941de48dc996d4051bf1.js"
 );
 
 self.addEventListener('message', (event) => {
